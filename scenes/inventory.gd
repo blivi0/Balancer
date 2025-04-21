8b0c4270f6aca1abbc7dd514f06d2690dfc65c58
David Amadeo <amadeo.k.david@gmail.com>
2025-04-21 00:29:36 -0500

    Implement tilting effect

--- scenes/inventory.gd
+++ scenes/inventory.gd
@@ -1,12 +1,24 @@
-extends VBoxContainer
+extends MarginContainer
 class_name Inventory
 
 const LEVEL_FORMAT := "res://scenes/levels/level_%d_%s.tscn"
+const TWEEN_DURATION := 0.75
 
-@onready var label: Label = $Label
+@onready var v_box_container: VBoxContainer = $VBoxContainer
+@onready var label: Label = $VBoxContainer/Label
 
 var slot_grid: SlotGrid
 var total: int
+var display_total: int
+var margin_top: int
+var tween: Tween
+
+signal tween_finished
+
+func _process(_delta: float) -> void:
+	if tween and tween.is_running():
+		label.text = str(display_total)
+		add_theme_constant_override("margin_top", margin_top)
 
 func load_level(level_num: int, side: String) -> void:
 	if slot_grid:
@@ -14,13 +26,32 @@ func load_level(level_num: int, side: String) -> void:
 	
 	var level_path := LEVEL_FORMAT % [level_num, side]
 	slot_grid = load(level_path).instantiate() as SlotGrid
-	add_child(slot_grid)
-	move_child(slot_grid, 0)
+	v_box_container.add_child(slot_grid)
+	v_box_container.move_child(slot_grid, 0)
 	update_total()
+	display_total = total
 
 func get_slots() -> Array[Slot]:
 	return slot_grid.slots
 
 func update_total() -> void:
 	total = slot_grid.get_total_weight()
-	label.text = str(total)
+
+func start_tween(target_margin_top: int) -> void:
+	if tween and tween.is_running():
+		tween.kill()
+	tween = get_tree().create_tween().set_parallel(true).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
+	
+	if abs(total - display_total) < 3:
+		display_total = total
+	else:
+		tween.tween_property(self, "display_total", total, TWEEN_DURATION)
+	
+	if margin_top != target_margin_top:
+		tween.tween_property(self, "margin_top", target_margin_top, TWEEN_DURATION)
+	
+	tween.chain().tween_callback(on_tween_finished)
+	
+func on_tween_finished() -> void:
+	tween = null
+	tween_finished.emit()
--- scenes/inventory_controller.gd
+++ scenes/inventory_controller.gd
@@ -1,14 +1,19 @@
 extends HBoxContainer
 class_name InventoryController
 
-@onready var left_container: MarginContainer = $LeftContainer
-@onready var right_container: MarginContainer = $RightContainer
-@onready var left_inventory: Inventory = $LeftContainer/LeftInventory
-@onready var right_inventory: Inventory = $RightContainer/RightInventory
+const MIN_MARGIN_TOP := 0
+const MAX_MARGIN_TOP := 96
+const MID_MARGIN_TOP = (MIN_MARGIN_TOP + MAX_MARGIN_TOP) / 2.0
+
+@onready var left_inventory: Inventory = $LeftInventory
+@onready var right_inventory: Inventory = $RightInventory
 
 var all_slots: Array[Slot]
-
 signal balanced
+
+func _ready() -> void:
+	# Only need one signal
+	left_inventory.tween_finished.connect(on_inventory_tween_finished)
 
 func load_level(level_num: int) -> void:
 	left_inventory.load_level(level_num, "left")
@@ -16,10 +21,25 @@ func load_level(level_num: int) -> void:
 	all_slots = left_inventory.get_slots() + right_inventory.get_slots()
 	for slot in all_slots:
 		slot.updated.connect(on_slot_updated)
+	start_inventory_tweens()
 
 func on_slot_updated() -> void:
 	left_inventory.update_total()
 	right_inventory.update_total()
+	start_inventory_tweens()
+
+func start_inventory_tweens() -> void:
+	if left_inventory.total == right_inventory.total:
+		left_inventory.start_tween(MID_MARGIN_TOP)
+		right_inventory.start_tween(MID_MARGIN_TOP)
+	elif left_inventory.total < right_inventory.total:
+		left_inventory.start_tween(MIN_MARGIN_TOP)
+		right_inventory.start_tween(MAX_MARGIN_TOP)
+	else:
+		left_inventory.start_tween(MAX_MARGIN_TOP)
+		right_inventory.start_tween(MIN_MARGIN_TOP)
+
+func on_inventory_tween_finished() -> void:
 	if left_inventory.total == right_inventory.total:
 		disable_slots()
 		balanced.emit()
