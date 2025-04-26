492527780c0c915cabc3e6e669229a076d92d4dd
David Amadeo <amadeo.k.david@gmail.com>
2025-04-26 02:27:24 -0500

    Add level and move numbers

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -3,8 +3,12 @@ extends Control
 @onready var inventory_controller: InventoryController = $InventoryController
 @onready var item_description: DescriptionContainer = $HBoxContainer/ItemDescription
 @onready var effect_description: DescriptionContainer = $HBoxContainer/EffectDescription
+@onready var level_num_label: Label = $LevelNumContainer/MarginContainer/LevelNumLabel
+@onready var move_num_label: Label = $MoveNumContainer/MarginContainer/MoveNumLabel
 @onready var win_menu: WinMenu = $WinMenu
 @onready var pause_menu: PauseMenu = $PauseMenu
+
+var num_moves: int
 
 func _ready() -> void:
 	inventory_controller.balanced.connect(on_inventory_balanced)
@@ -17,15 +21,23 @@ func _ready() -> void:
 
 func load_level() -> void:
 	win_menu.hide_win_menu()
+	num_moves = 0
+	move_num_label.text = "Moves: 0"
+	level_num_label.text = "Level: %d" % LevelManager.curr_level
 	inventory_controller.load_level(LevelManager.curr_level)
 	for slot in inventory_controller.all_slots:
+		slot.updated.connect(on_slot_updated)
 		slot.item_hovered.connect(on_slot_item_hovered)
 		slot.effect_hovered.connect(on_slot_effect_hovered)
 		slot.unhovered.connect(on_slot_unhovered)
 
-func on_inventory_balanced(num_moves: int) -> void:
+func on_inventory_balanced() -> void:
 	LevelManager.complete_level(num_moves)
 	win_menu.show_win_menu(num_moves)
+
+func on_slot_updated() -> void:
+	num_moves += 1
+	move_num_label.text = "Moves: %d" % num_moves
 
 func on_slot_item_hovered(slot_resource: SlotResource) -> void:
 	item_description.show_description(slot_resource)
--- scenes/game/inventory_controller.gd
+++ scenes/game/inventory_controller.gd
@@ -5,8 +5,7 @@ class_name InventoryController
 @onready var right_inventory: Inventory = $RightInventory
 
 var all_slots: Array[Slot]
-var num_moves := 0
-signal balanced(num_moves: int)
+signal balanced()
 
 func _ready() -> void:
 	# Only need one signal
@@ -14,7 +13,6 @@ func _ready() -> void:
 	left_inventory.move_weight_icon_first()
 
 func load_level(level_num: int) -> void:
-	num_moves = 0
 	left_inventory.load_level(level_num, "left")
 	right_inventory.load_level(level_num, "right")
 	all_slots = left_inventory.get_slots() + right_inventory.get_slots()
@@ -23,7 +21,6 @@ func load_level(level_num: int) -> void:
 	set_inventory_status()
 
 func on_slot_updated() -> void:
-	num_moves += 1
 	left_inventory.update_total()
 	right_inventory.update_total()
 	set_inventory_status()
@@ -42,7 +39,7 @@ func set_inventory_status() -> void:
 func on_inventory_tween_finished() -> void:
 	if left_inventory.total == right_inventory.total:
 		disable_slots()
-		balanced.emit(num_moves)
+		balanced.emit()
 
 func disable_slots() -> void:
 	for slot in all_slots:
